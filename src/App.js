319f8f920aab7b7292760df9620edb3745aba5ce
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2025-01-23 19:13:40 -0600

    push before breaking

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ import { ContextProvider } from './ContextProvider';
 import GraphContainer from "./components/GraphContainer";
 import { Amplify } from 'aws-amplify';
 import awsconfig from "./aws-exports";
+import React, { useState, useEffect } from 'react';
+import Popup from 'reactjs-popup';
 
 Amplify.configure(awsconfig);
 
 const App = () => {
+    const [privacyPolicyContent, setPrivacyPolicyContent] = useState('');
+
+    useEffect(() => {
+        // Fetch the privacy policy file from the public directory
+        fetch('/privacypolicy.txt')
+            .then(response => response.text()) // Read the text content of the file
+            .then(text => setPrivacyPolicyContent(text)) // Update state with the file's content
+            .catch(error => console.error('Error fetching privacy policy:', error)); // Handle errors
+    }, []);
 
     return (
         <ContextProvider>
             <div className="App">
-                <h1>Take the hassle out of buying a car</h1>
-                <div className="dropdown-container">
-                    <DropdownContainer />
+                <div className="body">
+                    <h1>Take the hassle out of buying a car</h1>
+                    <div className="dropdown-container">
+                        <DropdownContainer />
+                    </div>
+                    <div className="graph-container">
+                        <GraphContainer dataPoints />
+                    </div>
                 </div>
-                <div className="graph-container">
-                    <GraphContainer dataPoints />
+                <div className="footer">
+                    <div>
+                        <Popup trigger={<button className="popup-button">Privacy Policy</button>} modal >
+                            {close => (
+                                <div className="popup-content">
+                                    <p>{privacyPolicyContent}</p>
+                                    <button onClick={close}>Close</button>
+                                </div> 
+                            )}
+                        </Popup>
+                    </div>
+                    <p>&copy; 2025 Auto Hassle. All rights reserved.</p>
                 </div>
             </div>
         </ContextProvider>
---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PopupStyle.css';
+
+const Popup = () => {
+    return (
+        <div className="Popup">
+
+        </div>
+    );
+};
+
+export default Popup;
